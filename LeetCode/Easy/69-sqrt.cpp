d6fcacd727b2ca93962bdb3655959a4d5d70eac3
tusharRaghuwanshi43 <tusharraghuwanshi82@gmail.com>
2026-06-01 15:03:31 +0530

    sqrt & peak mountain problem added

---
+++ LeetCode/Easy/69-sqrt.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        int s = 0, e = x;
+        long long int mid = s + (e - s) / 2;
+        long long int ans = 0;
+        while (s <= e)
+        {
+            long long int square = mid * mid;
+            if (square == x)
+            {
+                return mid;
+            }
+            else if (square < x)
+            {
+                ans = mid;
+                s = 1 + mid;
+            }
+            else if (square > x)
+            {
+                e = mid - 1;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return ans;
+    }
+};
---
+++ LeetCode/Medium/852-peak-mountain-array.cpp
@@ -0,0 +1,44 @@
+// Binary Search
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int s = 0;
+        int e = arr.size() - 1;
+        int mid = s + (e - s) / 2;
+        while (s < e)
+        {
+            if (arr[mid] < arr[mid + 1])
+            {
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return s;
+    }
+};
+
+// Linear Search
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int max = INT_MIN;
+        int index;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+};
